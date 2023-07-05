14822215f56dccca145df0b94d0663ede33e306f
Sam Beer <sam.beer208@gmail.com>
2023-07-05 20:01:59 +0100

    first pass for args

--- lzw/src/main.rs
+++ lzw/src/main.rs
@@ -5,15 +5,36 @@ use std::io::{BufRead, BufReader, Read, Write};
 use tracing::{error, info, Level};
 use tracing_subscriber::FmtSubscriber;
 
+// TODO: do fancier exclusive fields? min and max code width only needed for variable width.
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 // #[command(propagate_version = true)]
 struct Args {
     #[arg(default_value = "encoded.txt")]
     filename: String,
+
+    #[arg(default_value_t = false)]
+    variable_width: bool,
+
+    #[arg(default_value_t = true)]
+    end_code: bool,
+
+    #[arg(default_value_t = 16)] // requires variable-width true
+    max_width: u8,
+
+    #[arg(default_value_t = 9)]
+    // requires variable-width true. Commonly inferred from alphabet rather than specified
+    min_width: u8,
+
+    #[arg(default_value_t = true)]
+    pack_msb_first: bool,
 }
 
 // https://planetcalc.com/9069/
+
+fn compress() {}
+
+fn decompress() {}
 
 fn b64_decode_from_file(filename: &str) -> std::io::Result<()> {
     // File is in b64 encoding
