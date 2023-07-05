310495f52c3a919debf00b6eb6fa1b777dc611b3
Sam Beer <sam.beer208@gmail.com>
2023-07-05 19:43:40 +0100

    prep work for b64 test case files

--- lzw/src/main.rs
+++ lzw/src/main.rs
@@ -1,4 +1,7 @@
+use base64::engine::general_purpose;
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
 use tracing::{error, info, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -6,18 +9,39 @@ use tracing_subscriber::FmtSubscriber;
 #[command(author, version, about, long_about=None)]
 // #[command(propagate_version = true)]
 struct Args {
-    #[arg(default_value_t = 10)]
-    arg_num: u8,
+    #[arg(default_value = "encoded.txt")]
+    filename: String,
+}
+
+// https://planetcalc.com/9069/
+
+fn b64_decode_from_file(filename: &str) -> std::io::Result<()> {
+    // File is in b64 encoding
+    let mut f = File::open(filename)?;
+    let mut decoder = base64::read::DecoderReader::new(&mut f, &general_purpose::STANDARD);
+
+    let mut result: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut result).unwrap();
+    let s_result = String::from_utf8(result).expect("Found invalid UTF-8");
+    println!("{}", s_result);
+    Ok(())
+}
+
+fn b64_encode_to_file(filename: &str) -> std::io::Result<()> {
+    let s = b"thetest";
+    let mut f: File = File::create(filename)?;
+    let mut encoder = base64::write::EncoderWriter::new(&mut f, &general_purpose::STANDARD);
+    encoder.write_all(s)?;
+    Ok(())
 }
 
 fn main() {
-    let subscriber = FmtSubscriber::builder()
+    let subscriber: FmtSubscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let args = Args::parse();
-    info!("Starting to do things, e.g. with arg_num: {}", args.arg_num);
-    // Do things
-    error!("Didn't do anything");
+    b64_encode_to_file(&args.filename).unwrap();
+    b64_decode_from_file(&args.filename).unwrap();
 }
