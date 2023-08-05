02c6a08e7cb8daa16d91a1cc21c896fe8a45ca2b
Sam Beer <sam.beer208@gmail.com>
2023-08-05 18:02:53 +0100

    build script and bannerimage

---
+++ yew-rocket/backend/data/README.md
@@ -0,0 +1,1 @@
+Download and unzip [Bootstrap](https://github.com/twbs/bootstrap/releases/download/v5.1.3/bootstrap-5.1.3-dist.zip)
--- yew-rocket/backend/src/main.rs
+++ yew-rocket/backend/src/main.rs
@@ -1,12 +1,29 @@
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use std::path::PathBuf;
+
 #[macro_use]
 extern crate rocket;
 
+async fn get_index() -> Result<NamedFile, NotFound<String>>{
+    NamedFile::open("../ui/dist/index.html").await.map_err(|e| NotFound(e.to_string()))
+}
+
+#[get("/<path..>")] // type safe way to create a path route
+async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>>{
+    let path = PathBuf::from("../ui/dist").join(path);
+    match NamedFile::open(path).await{
+        Ok(f) => Ok(f),
+        Err(_) => get_index().await, // redirect to index if it doesn't exist
+    }
+}
+
 #[get("/")]
-async fn index() -> String{
-    String::from("Hello World! 🦀")
+async fn index() -> Result<NamedFile, NotFound<String>>{
+    get_index().await
 }
 
 #[launch]
 fn rocket()-> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, static_files])
 }
